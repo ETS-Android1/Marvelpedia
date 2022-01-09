ed53cd12181a8d10ffb7201aef37a295f1070457
ml09 <mpachnou@athtech.gr>
2022-01-09 23:07:35 +0200

    Changes in ProfileFragment.java Changes in profile_fragment.xml upload marvel.jpg in drawable

--- app/src/main/java/com/project_future_2021/marvelpedia/fragments/ProfileFragment.java
+++ app/src/main/java/com/project_future_2021/marvelpedia/fragments/ProfileFragment.java
@@ -1,16 +1,20 @@
 package com.project_future_2021.marvelpedia.fragments;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.transition.MaterialFadeThrough;
 import com.project_future_2021.marvelpedia.R;
 import com.project_future_2021.marvelpedia.viewmodels.ProfileViewModel;
@@ -19,6 +23,12 @@ public class ProfileFragment extends Fragment {
 
     private static final String TAG = "ProfileFragment";
     private ProfileViewModel profileViewModel;
+    private Button editbutton;
+    private TextInputEditText firstname;
+    private TextInputEditText lastname;
+    private TextInputEditText email;
+    private TextInputEditText marvelname;
+    private Image image;
     private Button btnShare;
 
     public static ProfileFragment newInstance() {
@@ -34,25 +44,79 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_fragment);
 
-        MaterialFadeThrough exitTransition = new MaterialFadeThrough();
+        viewInitializations();
+    }
+
+    void viewInitializations() {
+        firstname = findViewById(R.id.linear_first_name);
+        email = findViewById(R.id.linear_email);
+        lastname = findViewById(R.id.linear_last_name);
+        marvelname = findViewById(R.id.linear_marvel_name);
+    }
+
+    private TextInputEditText findViewById(int linear_first_name) {
+        return;
+    }
+
+    boolean validateInput() {
+
+        if (firstname.getText().toString().equals("")) {
+            firstname.setError("Please Enter First Name");
+            return false;
+        }
+        if (email.getText().toString().equals("")) {
+            email.setError("Please Enter Email");
+            return false;
+        }
+        if (lastname.getText().toString().equals("")) {
+            lastname.setError("Please Enter Last Name");
+            return false;
+        }
+        if (marvelname.getText().toString().equals("")) {
+            marvelname.setError("Please Enter Marvel Name");
+            return false;
+        }
+
+        if (!isemailValid(email.getText().toString())) {
+            email.setError("Please Enter Valid Email");
+            return false;
+        }
+        return true;
+    }
+
+    boolean isemailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_fragment);
+        Button editbutton = (Button) findViewById(R.id.linear_edit);
+        Button btnShare = (Button) findViewById(R.id.btnShare);
 
-        btnShare = view.findViewById(R.id.btnShare);
-        btnShare.setOnClickListener(new View.OnClickListener() {
+        editbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // What happens when users click this button?
+            public void onClick(View view) {
+                Toast.makeText(this, "Edit", Toast.LENGTH_LONG).show();
             }
         });
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(this, "Share", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
-        // ignore these
+    private void setContentView(int profile_fragment) {
+    }
+}
+// ignore these
         /*TextView profile_txt = view.findViewById(R.id.profile_txt);
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -66,5 +130,7 @@ public class ProfileFragment extends Fragment {
         Glide.with(this)
                 .load(R.drawable.gif1)
                 .into(image);*/
-    }
-}
+
+
+
+
