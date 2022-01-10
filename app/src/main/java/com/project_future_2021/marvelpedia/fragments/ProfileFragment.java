0c3c291dbfc3da174501f1ba1b45f2700ed1b2f6
ml09 <mpachnou@athtech.gr>
2022-01-10 16:52:18 +0200

    Changes in ProfileFragment.java

--- app/src/main/java/com/project_future_2021/marvelpedia/fragments/ProfileFragment.java
+++ app/src/main/java/com/project_future_2021/marvelpedia/fragments/ProfileFragment.java
@@ -1,27 +1,22 @@
-        package com.project_future_2021.marvelpedia.fragments;
+package com.project_future_2021.marvelpedia.fragments;
 
-        import android.content.Context;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import android.preference.PreferenceManager;
-        import android.util.Patterns;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-        import androidx.annotation.NonNull;
-        import androidx.annotation.Nullable;
-        import androidx.fragment.app.Fragment;
-        import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-        import com.google.android.material.textfield.TextInputEditText;
-        import com.project_future_2021.marvelpedia.LoginActivity;
-        import com.project_future_2021.marvelpedia.R;
-        import com.project_future_2021.marvelpedia.viewmodels.ProfileViewModel;
+import com.project_future_2021.marvelpedia.R;
+import com.project_future_2021.marvelpedia.viewmodels.ProfileViewModel;
 
 public class ProfileFragment extends Fragment {
 
@@ -34,8 +29,6 @@ public class ProfileFragment extends Fragment {
     private TextView username;
     private TextView inputUsername;
     private Button btnSave;
-
-    private Bundle savedInstanceState;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -52,6 +45,51 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        viewInitializations();
+    }
+
+    void viewInitializations() {
+        firstname = findViewById(R.id.linear_first_name);
+        email = findViewById(R.id.linear_email);
+        lastname = findViewById(R.id.linear_last_name);
+        marvelname = findViewById(R.id.linear_marvel_name);
+    }
+
+
+    boolean validateInput() {
+
+        if (firstname.getText().toString().equals("")) {
+            firstname.setError("Please Enter First Name");
+            return false;
+        }
+        if (email.getText().toString().equals("")) {
+            email.setError("Please Enter Email");
+            return false;
+        }
+        if (lastname.getText().toString().equals("")) {
+            lastname.setError("Please Enter Last Name");
+            return false;
+        }
+        if (marvelname.getText().toString().equals("")) {
+            marvelname.setError("Please Enter Marvel Name");
+            return false;
+        }
+
+        if (!isemailValid(email.getText().toString())) {
+            email.setError("Please Enter Valid Email");
+            return false;
+        }
+        return true;
+    }
+
+    boolean isemailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         inputUsername = view.findViewById(R.id.input_username);
@@ -60,9 +98,9 @@ public class ProfileFragment extends Fragment {
         btnSave = view.findViewById(R.id.btn_save);
 
         SharedPreferences sp = requireActivity().getSharedPreferences("RegisterPrefs", Context.MODE_PRIVATE);
-        String registerUsername = sp.getString("registerUsername","");
-        String registerEmail = sp.getString("registerEmail","");
-        String registerName = sp.getString("registerName","");
+        String registerUsername = sp.getString("registerUsername", "");
+        String registerEmail = sp.getString("registerEmail", "");
+        String registerName = sp.getString("registerName", "");
 
         inputUsername.setText(registerUsername);
         inputEmail.setText(registerEmail);
@@ -79,15 +117,15 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                    String checkEmail = inputEmail.getText().toString();
-                    String checkName = inputName.getText().toString();
-                    String checkUsername = inputUsername.getText().toString();
+                String checkEmail = inputEmail.getText().toString();
+                String checkName = inputName.getText().toString();
+                String checkUsername = inputUsername.getText().toString();
 
-                    SharedPreferences.Editor editor = sp.edit();
-                    editor.putString("registerUsername", checkUsername);
-                    editor.putString("registerEmail", checkEmail);
-                    editor.putString("registerName", checkName);
-                    editor.commit();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("registerUsername", checkUsername);
+                editor.putString("registerEmail", checkEmail);
+                editor.putString("registerName", checkName);
+                editor.commit();
 
             }
         });
