1f9813eaa6445de8000634be315ffd7b9c489090
JohnStamatopoulos <johnstamatopoulos@hotmail.com>
2022-01-15 22:38:15 +0200

    01.Post-Presentation Day-Final Touches.
    Changed:
    - Changed min. api from 21 to 24.
    - Re-formatted some code (DetailsFragment).
    
    Project's requirements were met and on-time, going forward, there could be some updates! :D

--- app/src/main/java/com/project_future_2021/marvelpedia/fragments/DetailsFragment.java
+++ app/src/main/java/com/project_future_2021/marvelpedia/fragments/DetailsFragment.java
@@ -9,7 +9,6 @@ import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,137 +18,124 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.palette.graphics.Palette;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.project_future_2021.marvelpedia.R;
 import com.project_future_2021.marvelpedia.data.Hero;
-import com.project_future_2021.marvelpedia.viewmodels.DetailsViewModel;
 
 public class DetailsFragment extends Fragment {
 
     private static final String TAG = "DetailsFragment";
-    private DetailsViewModel detailsViewModel;
 
-    private Palette.Swatch vibrantSwatch;
-    private Palette.Swatch lightVibrantSwatch;
-    private Palette.Swatch darkVibrantSwatch;
+    Hero heroToShowDetailsFor;
+    ConstraintLayout details_base_layout;
+    TextView details_textview_hero_description;
+    TextView details_textview_hero_name;
+    ImageView details_imageview_hero_thumbnail;
+
+    /* The other possible swatches:
+        private Palette.Swatch vibrantSwatch;
+        private Palette.Swatch lightVibrantSwatch;
+        private Palette.Swatch darkVibrantSwatch;
+        private Palette.Swatch lightMutedSwatch;
+        private Palette.Swatch darkMutedSwatch;
+    */
+    // We went with this one, just personal preference.
     private Palette.Swatch mutedSwatch;
-    private Palette.Swatch lightMutedSwatch;
-    private Palette.Swatch darkMutedSwatch;
-    TextView textView;
 
-    ConstraintLayout constraintLayout;
-    TextView details_txt;
-    TextView name_txt;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    private int swatchNumber;
-
-    public static DetailsFragment newInstance() {
-        return new DetailsFragment();
+        // Have an animation for our shared-views, when they enter, and return.
+        Transition animation = TransitionInflater.from(requireContext()).inflateTransition(
+                android.R.transition.move
+        );
+        setSharedElementEnterTransition(animation);
+        setSharedElementReturnTransition(animation);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        detailsViewModel = new ViewModelProvider(this).get(DetailsViewModel.class);
         return inflater.inflate(R.layout.details_fragment, container, false);
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        /*Explode explode = new Explode();
-        requireActivity().getWindow().setExitTransition(explode);*/
-
-        // Have an animation for your shared-views, when the enter, and return.
-        Transition animation = TransitionInflater.from(requireContext()).inflateTransition(
-                android.R.transition.move
-        );
-        setSharedElementEnterTransition(animation);
-        setSharedElementReturnTransition(animation);
-
-
-        /*setSharedElementReturnTransition(TransitionInflater.from(getActivity()).inflateTransition(R.transition.change_image));
-        setExitTransition(TransitionInflater.from(getActivity()).inflateTransition(android.R.transition.explode));*/
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Get the arguments from sender-Fragment
-        DetailsFragmentArgs argsFromHeroesFragment = DetailsFragmentArgs.fromBundle(getArguments());
-        Hero heroToShowDetailsFor = argsFromHeroesFragment.getHeroForDetailsFragment();
+        hideNavigationBar();
 
-        /*TextView */
-        details_txt = view.findViewById(R.id.sharedTextViewHeroDescription);
-        /*TextView */
-        name_txt = view.findViewById(R.id.sharedTextViewHeroName);
-        ImageView thumbnail = view.findViewById(R.id.sharedImageViewHeroThumbnail);
+        receiveArgumentsFromSenderFragment();
 
-        name_txt.setText(heroToShowDetailsFor.getName());
-        details_txt.setText(heroToShowDetailsFor.getDescription());
+        initViews(view);
+    }
 
-        /*Glide.with(this)
-                .load(heroToShowDetailsFor.getThumbnail().makeImageWithVariant("portrait_xlarge"))
-                //.placeholder(R.drawable.ic_baseline_image_search_24)
-                .into(thumbnail);*/
-
-        //make the bottom navigation bar invisible
+    private void hideNavigationBar() {
+        // Make the bottom navigation bar invisible.
         requireActivity().findViewById(R.id.main_btm_nav_view).setVisibility(View.GONE);
+    }
 
-        /*textView = view.findViewById(R.id.textView);
-        Button btnSwatch = view.findViewById(R.id.buttonSwatch);*/
-        constraintLayout = view.findViewById(R.id.details_base_layout);
-        /*btnSwatch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nextSwatch(v);
-            }
-        });*/
-        //ImageView myImage = view.findViewById(R.id.detailsMyImage);
-        //Bitmap bitmap = ((BitmapDrawable) myImage.getDrawable()).getBitmap();
-        //Bitmap bitmap = ((BitmapDrawable) myImage.getDrawable()).getBitmap();
-        //Bitmap bitmap = BitmapFactory.decodeStream(requireActivity().getContentResolver().openInputStream(heroToShowDetailsFor.getThumbnail().makeImageWithVariant("portrait_xlarge")));
+    private void receiveArgumentsFromSenderFragment() {
+        // Get the arguments from sender-Fragment.
+        DetailsFragmentArgs argsFromHeroesFragment = DetailsFragmentArgs.fromBundle(getArguments());
+        heroToShowDetailsFor = argsFromHeroesFragment.getHeroForDetailsFragment();
+    }
 
+    private void initViews(View view) {
+        details_base_layout = view.findViewById(R.id.details_base_layout);
+        details_textview_hero_name = view.findViewById(R.id.sharedTextViewHeroName);
+        details_textview_hero_description = view.findViewById(R.id.sharedTextViewHeroDescription);
+        details_imageview_hero_thumbnail = view.findViewById(R.id.sharedImageViewHeroThumbnail);
 
+        details_textview_hero_name.setText(heroToShowDetailsFor.getName());
+        details_textview_hero_description.setText(heroToShowDetailsFor.getDescription());
+
+        // Load the image with the help of Glide
         Glide.with(this)
                 .asBitmap()
                 .load(heroToShowDetailsFor.getThumbnail().makeImageWithVariant("portrait_xlarge"))
-                .into(new BitmapImageViewTarget(thumbnail) {
+                .into(new BitmapImageViewTarget(details_imageview_hero_thumbnail) {
                     @Override
-                    public void onResourceReady(@NonNull Bitmap mbitmap, @Nullable com.bumptech.glide.request.transition.Transition<? super Bitmap> transition) {
-                        super.onResourceReady(mbitmap, transition);
+                    public void onResourceReady(@NonNull Bitmap bitmap, @Nullable com.bumptech.glide.request.transition.Transition<? super Bitmap> transition) {
+                        super.onResourceReady(bitmap, transition);
 
-                        Palette.generateAsync(mbitmap, 64, new Palette.PaletteAsyncListener() {
+                        // Create the palette Asynchronously, with 64 colors (64 is actually a lot, usually 32 or even 16 is enough...)
+                        Palette.generateAsync(bitmap, 64, new Palette.PaletteAsyncListener() {
                             @Override
                             public void onGenerated(@Nullable Palette palette) {
+                                // If the palette successfully "found" colors from the thumbnail Image.
                                 if (palette != null) {
-                                    vibrantSwatch = palette.getVibrantSwatch();
-                                    lightVibrantSwatch = palette.getLightVibrantSwatch();
-                                    darkVibrantSwatch = palette.getDarkVibrantSwatch();
+                                    /*
+                                        vibrantSwatch = palette.getVibrantSwatch();
+                                        lightVibrantSwatch = palette.getLightVibrantSwatch();
+                                        darkVibrantSwatch = palette.getDarkVibrantSwatch();
+                                        lightMutedSwatch = palette.getLightMutedSwatch();
+                                        darkMutedSwatch = palette.getDarkMutedSwatch();
+                                    */
                                     mutedSwatch = palette.getMutedSwatch();
-                                    lightMutedSwatch = palette.getLightMutedSwatch();
-                                    darkMutedSwatch = palette.getDarkMutedSwatch();
 
-
-//here we set the background color
+                                    // If the palette successfully "found" colors from the thumbnail Image for the mutedSwatch variation.
                                     if (mutedSwatch != null) {
-                                        constraintLayout.setBackgroundColor(mutedSwatch.getRgb());
-                                        name_txt.setTextColor(mutedSwatch.getTitleTextColor());
-                                        details_txt.setTextColor(mutedSwatch.getTitleTextColor());
+
+                                        // Change the base layout background color.
+                                        details_base_layout.setBackgroundColor(mutedSwatch.getRgb());
+                                        // Change the hero name text color.
+                                        details_textview_hero_name.setTextColor(mutedSwatch.getTitleTextColor());
+                                        // Change the hero description text color.
+                                        details_textview_hero_description.setTextColor(mutedSwatch.getTitleTextColor());
 
                                         ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
                                         if (actionBar != null) {
                                             actionBar.setDisplayHomeAsUpEnabled(true);
-                                            //set color action bar
+                                            // Change the color of the Action bar.
                                             actionBar.setBackgroundDrawable(new ColorDrawable(manipulateColor(mutedSwatch.getRgb(), 0.62f)));
 
-                                            //set color status bar
+                                            // Change the color of the Status bar.
                                             requireActivity().getWindow().setStatusBarColor(manipulateColor(mutedSwatch.getRgb(), 0.32f));
                                         }
                                     }
@@ -158,91 +144,10 @@ public class DetailsFragment extends Fragment {
                         });
                     }
                 });
-
-
-
-
-
-
-        /*Palette.from(bitmap)*//*.maximumColorCount(16)*//*.generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(@Nullable Palette palette) {
-                if (palette != null) {
-                    vibrantSwatch = palette.getVibrantSwatch();
-                    lightVibrantSwatch = palette.getLightVibrantSwatch();
-                    darkVibrantSwatch = palette.getDarkVibrantSwatch();
-                    mutedSwatch = palette.getMutedSwatch();
-                    lightMutedSwatch = palette.getLightMutedSwatch();
-                    darkMutedSwatch = palette.getDarkMutedSwatch();
-                }
-            }
-        });*/
     }
 
-    public void nextSwatch(View v) {
-        Palette.Swatch currentSwatch = null;
-        switch (swatchNumber) {
-            case 0:
-                currentSwatch = vibrantSwatch;
-                textView.setText("vibrantSwatch");
-                break;
-            case 1:
-                currentSwatch = lightVibrantSwatch;
-                textView.setText("lightVibrantSwatch");
-                break;
-            case 2:
-                currentSwatch = darkVibrantSwatch;
-                textView.setText("darkVibrantSwatch");
-                break;
-            case 3:
-                currentSwatch = mutedSwatch;
-                textView.setText("mutedSwatch");
-                break;
-            case 4:
-                currentSwatch = lightMutedSwatch;
-                textView.setText("lightMutedSwatch");
-                break;
-            case 5:
-                currentSwatch = darkMutedSwatch;
-                textView.setText("darkMutedSwatch");
-                break;
-        }
-        if (currentSwatch != null) {
-            //constraintLayout.setBackgroundColor(currentSwatch.getRgb());
-            //textView.setTextColor(currentSwatch.getTitleTextColor());
-
-            constraintLayout.setBackgroundColor(currentSwatch.getRgb());
-            name_txt.setTextColor(currentSwatch.getTitleTextColor());
-            details_txt.setTextColor(currentSwatch.getTitleTextColor());
-
-
-            //ActionBar actionBar = requireActivity().getActionBar();
-
-            ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.setDisplayHomeAsUpEnabled(true);
-                //set color action bar
-                actionBar.setBackgroundDrawable(new ColorDrawable(manipulateColor(currentSwatch.getRgb(), 0.62f)));
-
-                //set color status bar
-                requireActivity().getWindow().setStatusBarColor(manipulateColor(currentSwatch.getRgb(), 0.32f));
-            }
-        } else {
-            //constraintLayout.setBackgroundColor(Color.WHITE);
-            //textView.setTextColor(Color.RED);
-            constraintLayout.setBackgroundColor(Color.WHITE);
-            name_txt.setTextColor(Color.RED);
-            details_txt.setTextColor(Color.RED);
-        }
-
-        if (swatchNumber < 5) {
-            swatchNumber++;
-        } else {
-            swatchNumber = 0;
-        }
-    }
-
-    public static int manipulateColor(int color, float factor) {
+    // Create a slight variation of the given color.
+    private static int manipulateColor(int color, float factor) {
         int a = Color.alpha(color);
         int r = Math.round(Color.red(color) * factor);
         int g = Math.round(Color.green(color) * factor);
--- app/src/main/java/com/project_future_2021/marvelpedia/singletons/VolleySingleton.java
+++ app/src/main/java/com/project_future_2021/marvelpedia/singletons/VolleySingleton.java
@@ -10,28 +10,11 @@ public class VolleySingleton {
 
     private static VolleySingleton instance;
     private RequestQueue requestQueue;
-    //private ImageLoader imageLoader;
     private static Context ctx;
 
     private VolleySingleton(Context context) {
         ctx = context;
         requestQueue = getRequestQueue();
-
-        /*imageLoader = new ImageLoader(requestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });*/
     }
 
     public static synchronized VolleySingleton getInstance(Context context) {
@@ -53,9 +36,5 @@ public class VolleySingleton {
     public <T> void addToRequestQueue(Request<T> req) {
         getRequestQueue().add(req);
     }
-
-    /*public ImageLoader getImageLoader() {
-        return imageLoader;
-    }*/
 }
 
